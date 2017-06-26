532dbc0ad69dceed29ea2e954aa21ee1d4bb27d1
Greg Schofield <gregs@indellient.com>
2017-06-26 15:48:37 -0400

    Added more spec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,8 +6,12 @@
 
 require 'spec_helper'
 
+ChefSpec.define_matcher :my_custom_resource
+
 describe 'spring_service::default' do
+
   context 'When all attributes are default, on an unspecified platform' do
+
     let(:chef_run) do
       ChefSpec::SoloRunner.new(step_into: ['spring_service'], cookbook_path: ['./test/cookbooks', '../'], platform: 'centos', version: '6.7') do |node|
       end.converge('spring_service_test::default')
@@ -16,5 +20,14 @@ describe 'spring_service::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'creates a symbolic link' do
+      expect(chef_run).to create_link('/testdir/test.jar').with(link_type: :symbolic)
+    end
+
+    it 'creates a template with the default action' do
+	  expect(chef_run).to create_template('/testdir/test.conf').with(mode: 755)
+	end
+
   end
 end
--- test/cookbooks/spring_service_test/recipes/default.rb
+++ test/cookbooks/spring_service_test/recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 spring_service 'test' do
-	artifact_directory 'test'
+	artifact_directory '/testdir'
 	artifact 'test.jar'
+	java_opts '-xmx512m'
 end
