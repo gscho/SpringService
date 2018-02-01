9e229cbd02dff13c54154e52b977342ac329d008
GSCHOF11 <gregs@indellient.com>
2018-02-01 14:00:19 -0500

    Removed deprecated custom resource code

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,14 @@
 name 'spring_service'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Greg Schofield'
+maintainer_email 'gregs@indellient.com'
 license 'all_rights'
 description 'Installs/Configures spring_service'
 long_description 'Installs/Configures spring_service'
-version '0.1.0'
+version '0.2.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/spring_service/issues' if respond_to?(:issues_url)
+def url(name)
+  "https://github.com/gscho/spring_service/#{name}"
+end
 
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/spring_service' if respond_to?(:source_url)
+issues_url url issues if respond_to?(:issues_url)
+source_url url spring_service if respond_to?(:source_url)
--- resources/spring_service.rb
+++ resources/spring_service.rb
@@ -1,69 +1,67 @@
 actions :default
 default_action :default
 
-property :artifact_directory, :name_attribute => false, :kind_of => String, :required => true
-property :artifact, :name_attribute => false, :kind_of => String, :required => true
+property :artifact_directory,  :kind_of => String, :required => true
+property :artifact, :kind_of => String, :required => true
+property :service_name, :name_attribute => true, :kind_of => String, :required => true
 
-property :jar_file, :name_attribute => false, :kind_of => String, :required => false
-property :log_folder, :name_attribute => false, :kind_of => String, :required => false
-property :run_args, :name_attribute => false, :kind_of => String, :required => false
-property :java_opts, :name_attribute => false, :kind_of => String, :required => false
-property :log_filename, :name_attribute => false, :kind_of => String, :required => false, :default => "#{artifact}.log"
+property :jar_file, :kind_of => String, :required => false
+property :log_folder, :kind_of => String, :required => false
+property :run_args, :kind_of => String, :required => false
+property :java_opts, :kind_of => String, :required => false
+property :log_filename, :kind_of => String, :required => false
 
-property :mode, :name_attribute => false, :kind_of => Integer, :required => false
-property :user, :name_attribute => false, :kind_of => String, :required => false
-property :service_type, :name_attribute => false, :kind_of => String, :required => false
+property :mode, :kind_of => Integer, :required => false, default: 755
+property :user, :kind_of => String, :required => false, default: 'root'
+property :service_type, :kind_of => String, :required => false
 
 resource_name 'spring_service'
 
 action :default do
 
-  artifact_name = ::File.basename(artifact, ::File.extname(artifact))
-
-  if service_type.nil? then
+  type = new_resource.service_type
+  if type.nil? then
     if ::File.directory?('/usr/lib/systemd') then
-      service_type = 'systemd'
+      type = 'systemd'
     elsif ::File.directory?('/etc/init.d') then
-      service_type = 'init.d'
+      type = 'init.d'
     end
   end
 
-  user = user.nil? ? 'root' : user
-  mode = mode.nil? ? 755 : mode
 
-  if service_type == 'init.d' then
-    link "#{artifact_directory}/#{artifact}" do
-      to "/etc/init.d/#{artifact_name}"
+  if type == 'init.d' then
+    link "#{new_resource.artifact_directory}/#{new_resource.artifact}" do
+      to "/etc/init.d/#{new_resource.service_name}"
       link_type :symbolic
-      mode mode
+      mode new_resource.mode
     end
-  elsif service_type == 'systemd' then
-    template "/etc/systemd/system/#{artifact_name}.service" do
+  elsif type == 'systemd' then
+    template "/etc/systemd/system/#{new_resource.service_name}.service" do
       source 'systemd.service.erb'
-      mode mode
+      mode new_resource.mode
       variables({
-        :desc => "#{artifact} service script",
+        :desc => "#{new_resource.artifact} service script",
         :user => user,
-        :exec_start => "#{artifact_directory}/#{artifact}"
+        :exec_start => "#{new_resource.artifact_directory}/#{new_resource.artifact}"
       })
     end
   else
-    raise "Unrecognized service type: #{service_type}"
+    raise "Unrecognized service type: #{type}"
   end
 
-  template "#{artifact_directory}/#{artifact_name}.conf" do
+  template "#{new_resource.artifact_directory}/#{new_resource.service_name}.conf" do
     source 'artifact.conf.erb'
-    mode mode
+    mode new_resource.mode
     variables({
-      :jar_file => jar_file,
-      :log_folder => log_folder,
-      :run_args => run_args,
-      :java_opts => java_opts,
-      :log_filename => log_filename
+      :jar_file => new_resource.jar_file,
+      :log_folder => new_resource.log_folder,
+      :run_args => new_resource.run_args,
+      :java_opts => new_resource.java_opts,
+      :log_filename => new_resource.log_filename
     })
   end
 
-  service artifact do
+  service new_resource.artifact do
     action :start
   end
 end
